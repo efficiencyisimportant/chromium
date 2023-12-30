442778ea312548c76900690c87d7da39ca841cf7
Gabriele Colombo <efficiencyisimportant@gmail.com>
2023-12-30 13:44:39 +0100

    Remove root_store.pb.h extra include

--- net/cert/internal/trust_store_chrome.h
+++ net/cert/internal/trust_store_chrome.h
@@ -7,10 +7,13 @@
 
 #include "base/containers/span.h"
 #include "net/base/net_export.h"
-#include "net/cert/root_store_proto_lite/root_store.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/boringssl/src/pki/trust_store.h"
 #include "third_party/boringssl/src/pki/trust_store_in_memory.h"
+
+namespace chrome_root_store {
+class RootStore;
+}
 
 namespace net {
 
