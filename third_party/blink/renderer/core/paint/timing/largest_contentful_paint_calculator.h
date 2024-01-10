7af06fbfd3bbc6ea4499419280c00d16b166b6d3
Gabriele Colombo <efficiencyisimportant@gmail.com>
2024-01-10 09:27:31 +0100

    Remove paint_timing_detector.h extra include

--- third_party/blink/renderer/core/paint/timing/largest_contentful_paint_calculator.h
+++ third_party/blink/renderer/core/paint/timing/largest_contentful_paint_calculator.h
@@ -12,6 +12,9 @@
 
 namespace blink {
 
+class ImageRecord;
+class TextRecord;
+
 // LargestContentfulPaintCalculator is responsible for tracking the largest
 // image paint and the largest text paint and notifying WindowPerformance
 // whenever a new LatestLargestContentfulPaint entry should be dispatched.
--- third_party/blink/renderer/core/timing/performance_timing_for_reporting.h
+++ third_party/blink/renderer/core/timing/performance_timing_for_reporting.h
@@ -10,7 +10,6 @@
 #include "third_party/blink/public/web/web_performance_metrics_for_reporting.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
-#include "third_party/blink/renderer/core/paint/timing/paint_timing_detector.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
 
@@ -22,6 +21,7 @@ class DocumentParserTiming;
 class DocumentTiming;
 class InteractiveDetector;
 class PaintTiming;
+class PaintTimingDetector;
 struct LargestContentfulPaintDetails;
 
 // This class is only used for non-web-exposed reporting purposes (e.g. UKM).
